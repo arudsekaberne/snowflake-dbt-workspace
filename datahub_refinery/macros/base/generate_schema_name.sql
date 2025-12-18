34e9c809e4c7525b732a752a0b67f8cfe5b39ec4
ARUDSEKABERNESNOWEXP3 <arudsekaberne.learn@gmail.com>
2025-12-18 03:30:39 +0000

    Dynamic schema generation based on model folder commit

--- datahub_refinery/macros/base/generate_schema_name.sql
+++ datahub_refinery/macros/base/generate_schema_name.sql
@@ -1,14 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
-    {%- endif -%}
+    {# Dynamically generates the schema name based on the model’s directory structure. #}
+    {{ node.original_file_path.strip().upper().split('/')[-2] }}
 
 {%- endmacro %}
